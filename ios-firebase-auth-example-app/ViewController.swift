0d281fc9d783a4f4bfb0f7a5601c853d982851de
aadzhanow <125435070+aadzhanow@users.noreply.github.com>
2024-05-16 10:29:54 +0200

    Initial Commit

---
+++ ios-firebase-auth-example-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-firebase-auth-example-app
+//
+//  Created by Alisher on 16.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
